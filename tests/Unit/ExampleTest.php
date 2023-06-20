d47d8d57aa509640fe0800e701148a1f56fa0c35
Ivan Andres L <ivan.lopez@evertecinc.com>
2023-06-20 17:12:29 -0500

    Create ExampleTest.php

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Unit;
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+
+    public function test_example_test()
+    {
+        $this->assertTrue(true);
+    }
+}
