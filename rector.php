2e44e19c6d89569bd26b420d8cd209b2fbc0baf0
Iván Andrés López G. <andresgom11012@gmail.com>
2024-11-19 09:27:42 -0500

    feat: Upgrades runtime to php 8.3

--- rector.php
+++ rector.php
@@ -4,24 +4,17 @@ declare(strict_types=1);
 
 use Rector\Caching\ValueObject\Storage\FileCacheStorage;
 use Rector\Config\RectorConfig;
-use Rector\Php73\Rector\FuncCall\JsonThrowOnErrorRector;
 use Rector\Set\ValueObject\LevelSetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__.'/src',
         __DIR__.'/tests',
-    ]);
+    ])
+    ->withRules([
+        InlineConstructorDefaultToPropertyRector::class,
+    ])->withSets([
+        LevelSetList::UP_TO_PHP_83,
+    ])
+    ->withCache('./.rector-cache', FileCacheStorage::class);
 
-    // define sets of rules
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_82,
-    ]);
-
-    $rectorConfig->skip([
-        JsonThrowOnErrorRector::class,
-    ]);
-
-    $rectorConfig->cacheClass(FileCacheStorage::class);
-    $rectorConfig->cacheDirectory('./.rector-cache');
-};
